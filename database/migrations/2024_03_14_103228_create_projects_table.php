085efa42b4d1c2427b82ab3f0ecf781a4c0dc4c5
JayJairam <jmojairam@gmail.com>
2024-03-15 10:25:42 +0100

    Update 2024_03_14_103228_create_projects_table.php

--- database/migrations/2024_03_14_103228_create_projects_table.php
+++ database/migrations/2024_03_14_103228_create_projects_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->text('description')->nullable();
             $table->boolean('active')->default(false);
             $table->timestamps();
-            
+            $table->string('image')->nullable();
         });
     }
 
