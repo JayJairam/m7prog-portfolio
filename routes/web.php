cb371a9bde2debda374eaa1132f92b71bc14e679
JayJairam <jmojairam@gmail.com>
2024-03-28 11:45:29 +0100

    Revert "les 5"
    This reverts commit 8895d7bf79745ae4f45e8915057826829226e87e.

--- routes/web.php
+++ routes/web.php
@@ -28,13 +28,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/projects/add', [ ProjectController::class, 'add' ])->name('project.add');
-
 });
 
 require __DIR__.'/auth.php';
 
 Route::get('/About', [ \App\Http\Controllers\AboutController::class, 'index'])->name('About');
-Route::get('/Project', [ \App\Http\Controllers\ProjectController::class, 'index'])->name('Project');
 
 Route::get('/projects/index', [ ProjectController::class, 'index' ])->name('project.index');
