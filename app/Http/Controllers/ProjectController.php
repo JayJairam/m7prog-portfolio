11107b3c5d20d8ed2d5e7ace590b8b85b1404d74
JayJairam <jmojairam@gmail.com>
2024-03-28 12:00:47 +0100

    Revert "added things"
    This reverts commit 535b86e6ffdac2f34aa14e57546a65f66f3e610d.

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,29 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Project;
 
 class ProjectController extends Controller
 {
     public function index() {
+        return 'Dit is de about pagina.';
         $projects = Project::all();
+        
         return view(
-            'project.index',
+            'projects.index',
             [
                 'projects' => $projects,
             ]
-        );
+);
+
+
     }
-    
 
     public function add() {
-        // Maak een model aan
-        $model = new Project();
-        // definieer de velden
-        $model->field_one = 'mijn data';
-        // sla het model op
-        $model->save();
-
-        
+            // Maak een model aan
+    $model = new Model();
+    // definieer de velden
+    $model->field_one = 'mijn data';
+    // sla het model op
+    $model->save();
     }
 }
+
